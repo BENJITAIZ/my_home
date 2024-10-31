a288f75d51cb479baa091b5a37c23baaf41136ec
TOTO <taizieres.esead@gmail.com>
2024-10-31 09:20:55 +0100

    capteur humidité OK

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/adc.h>
 #include "LCD.h"
 
 #define LED_YELLOW_NODE DT_ALIAS(led_yellow)
@@ -13,15 +14,101 @@ const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE,
 static const struct i2c_dt_spec lcd_screen = I2C_DT_SPEC_GET(LCD_SCREEN_NODE);
 
 #define SLEEP_TIME_MS 10000  
+#define ADC_NODE DT_PATH(humidity_sensor)  
+#define ADC_RESOLUTION 12                 
+#define ADC_GAIN ADC_GAIN_1
+#define ADC_REFERENCE ADC_REF_INTERNAL
+#define ADC_ACQUISITION_TIME ADC_ACQ_TIME_DEFAULT
+#define ADC_CHANNEL_ID 0
+#define ADC_BUFFER_SIZE 1
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+/* Data of ADC io-channels specified in devicetree. */
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
+
+static const struct device *adc_dev;
+static int16_t adc_buffer[ADC_BUFFER_SIZE];
+
+
+void read_analog() {
+    	int err;
+	uint32_t count = 0;
+	uint16_t buf;
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+		if (!adc_is_ready_dt(&adc_channels[i])) {
+			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+			return 0;
+		}
+
+		err = adc_channel_setup_dt(&adc_channels[i]);
+		if (err < 0) {
+			printk("Could not setup channel #%d (%d)\n", i, err);
+			return 0;
+		}
+	}
+
+	for (int k = 0; k < 10; k++) {
+		printk("ADC reading[%u]:\n", count++);
+		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+			int32_t val_mv;
+
+			printk("- %s, channel %d: ",
+			       adc_channels[i].dev->name,
+			       adc_channels[i].channel_id);
+
+			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+
+			err = adc_read_dt(&adc_channels[i], &sequence);
+			if (err < 0) {
+				printk("Could not read (%d)\n", err);
+				continue;
+			}
+
+			/*
+			 * If using differential mode, the 16 bit value
+			 * in the ADC sample buffer should be a signed 2's
+			 * complement value.
+			 */
+			if (adc_channels[i].channel_cfg.differential) {
+				val_mv = (int32_t)((int16_t)buf);
+			} else {
+				val_mv = (int32_t)buf;
+			}
+			printk("%"PRId32, val_mv);
+			err = adc_raw_to_millivolts_dt(&adc_channels[i],
+						       &val_mv);
+			/* conversion to mV may not be supported, skip if not */
+			if (err < 0) {
+				printk(" (value in mV not available)\n");
+			} else {
+				printk(" = %"PRId32" mV\n", val_mv);
+			}
+		}
+
+		
+	}
+}
 
 int main(void) {
+ 
     if (gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH) < 0) {
         printk("Failed to configure LED pin\n");
-        return;
+        return -1;
     }
-//GOOD
  
-    printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+    printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
    
     init_lcd(&lcd_screen);
@@ -30,39 +117,16 @@ int main(void) {
     const struct device *dht11 = DEVICE_DT_GET_ONE(aosong_dht);
     if (!device_is_ready(dht11)) {
         printk("DHT11 sensor not ready\n");
-        return;
+        return -1;
     }
-
     printk("DHT11 sensor is ready\n");
 
-   
-    while (1) {
- 
-    if (sensor_sample_fetch(dht11) < 0) {
-        printk("Failed to fetch data from sensor\n");
-        continue;
+    while(1) {
+        read_analog();
+        k_sleep(K_SECONDS(1));
     }
-
-    struct sensor_value temp, humidity;
-
-
-    if (sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0) {
-        printk("Failed to get temperature\n");
-        continue;
-    }
-
-
-    if (sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity) < 0) {
-        printk("Failed to get humidity\n");
-        continue;
-    }
-
- 
     
 
-    printk("Temperature: %d.%06d C, Humidity: %d.%06d%%\n", temp.val1, temp.val2, humidity.val1,humidity.val2 );
 
-    k_msleep(SLEEP_TIME_MS);
+    return 0;
 }
-}
-
